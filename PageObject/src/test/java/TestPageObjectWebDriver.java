9566e860f9ea7ad613807bcaa2e57d6ba78ca311
Dmitry Asonov <asdmitrij@mail.ru>
2020-12-13 12:27:53 +0300

    Update TestPageObjectWebDriver.java

--- PageObject/src/test/java/TestPageObjectWebDriver.java
+++ PageObject/src/test/java/TestPageObjectWebDriver.java
@@ -1,7 +1,7 @@
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
